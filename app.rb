ea20d962c489788289620ccd245a4c9bd88d2bc0
JC <bonassin@gmail.com>
2021-03-09 16:31:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do 
+    "My name is Michael Jackson" 
+  end 
+
+  get '/hometown' do 
+    "My hometown is Gary"
+  end 
+
+  get "/favorite-song" 
+  "My favorite song is your favorite song ha!"   
+end 
+  
 end
